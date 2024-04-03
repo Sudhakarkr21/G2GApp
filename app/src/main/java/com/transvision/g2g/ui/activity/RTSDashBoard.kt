28586ce725b36f445b2366af8c4ca0149589d7c2
Sudhakarkr21 <sudhakarkr21@gmail.com>
2024-04-03 21:25:39 +0530

    Added Activity

---
+++ app/src/main/java/com/transvision/g2g/ui/activity/RTSDashBoard.kt
@@ -0,0 +1,12 @@
+package com.transvision.g2g.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.transvision.g2g.R
+
+class RTSDashBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rtsdash_board)
+    }
+}
--- app/src/main/java/com/transvision/g2g/ui/screen/dashboard/tabs/HomeTab.kt
+++ app/src/main/java/com/transvision/g2g/ui/screen/dashboard/tabs/HomeTab.kt
@@ -1,5 +1,6 @@
 package com.transvision.g2g.ui.screen.dashboard.tabs
 
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -20,62 +21,64 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.transvision.g2g.ui.activity.RTSDashBoard
 import com.transvision.g2g.ui.screen.NavigationRoutes
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview(showBackground = true)
 fun HomeContent(navController: NavController) {
+    val context = LocalContext.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
         Spacer(modifier = Modifier.padding(10.dp))
-        Card(onClick = {
+        cardView("MIS Dashboard", onClick = {
             navController.navigate(NavigationRoutes.Authenticated.MISDashBoard.route)
+        })
+        cardView("RTI Dashboard", onClick = {
+            context.startActivity(
+                Intent(
+                    context,
+                    RTSDashBoard::class.java
+                )
+            )
+        })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun cardView(title: String, onClick: () -> Unit) {
+    Card(
+        onClick = {
+            onClick()
         },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .padding(6.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
             modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp)
-                .padding(6.dp)
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(horizontalArrangement = Arrangement.Start,
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Spacer(modifier = Modifier.padding(start = 10.dp))
-                Icon(
-                    imageVector = Icons.Filled.Dashboard,
-                    contentDescription = "MIS"
-                )
-                Spacer(modifier = Modifier.padding(start = 10.dp))
-                Text(text = "MIS Dashboard")
-            }
-        }
-        Card(onClick = {  },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp)
-                .padding(6.dp)
-        ) {
-            Row(horizontalArrangement = Arrangement.Start,
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Spacer(modifier = Modifier.padding(start = 10.dp))
-                Icon(
-                    imageVector = Icons.Filled.Dashboard,
-                    contentDescription = "MIS"
-                )
-                Spacer(modifier = Modifier.padding(start = 10.dp))
-                Text(text = "RTI Dashboard")
-            }
+            Spacer(modifier = Modifier.padding(start = 10.dp))
+            Icon(
+                imageVector = Icons.Filled.Dashboard,
+                contentDescription = "MIS"
+            )
+            Spacer(modifier = Modifier.padding(start = 10.dp))
+            Text(text = title)
         }
     }
 }
